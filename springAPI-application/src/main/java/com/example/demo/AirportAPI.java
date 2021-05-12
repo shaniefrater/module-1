7550697daa358aabdea3334a1f632928a05b945b
shaniefrater <shaniemarie@live.co.uk>
2021-05-12 16:10:52 +0100

    Implementation Testing

--- springAPI-application/src/main/java/com/example/demo/AirportAPI.java
+++ springAPI-application/src/main/java/com/example/demo/AirportAPI.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,33 +10,52 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 @RestController
 public class AirportAPI {
 
-    @GetMapping(value = {"/globalDirectory"})
-    public String globalDirectory() {
-        return("Welcome to the Global Airports System");
-    }
+    @GetMapping(value = {"/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String siteHome() {
 
-    @PostMapping(value = {"/airportList"})
-    public String airportList() {
-        return("Please see below airports");
+        return("{\"message\":\"Welcome to the Global Airports System\"}");
     }
+    // get all airports (200 / 404-400)
 
-    @PutMapping(value = {"/updateAirportList"})
-    public String updateAirportList() {
-        return("Select the airport you wish to amend");
+    @GetMapping(value = {"/airports"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAirports() {
+
+        return("{\"message\":\"View all airports data\"}");
     }
+    // get all airports (200 / 404-400)
 
-    @DeleteMapping(value = {"/deleteAirports"})
+    @GetMapping(value = {"/airports/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAirport() {
+
+        return("{\"message\":\"You are now viewing {id}\"}");
+    }
+    // retrieve specific airport data (200 / 400)
+
+    @PostMapping(value = {"/airports"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String addAirport() {
+
+        return("{\"message\":\"Please select which airport to add to the system\"}");
+    }
+    // create an airport in the collection (201)
+
+    @PutMapping(value = {"/airports/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String updateAirport() {
+
+        return("{\"message\":\"Select the airport you wish to amend\"}");
+    }
+    // updating a specific airport (200-204)
+
+    @DeleteMapping(value = {"/airports"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public String deleteAirports() {
-        return("Delete entire airport list");
-    }
 
-    @PutMapping(value = {"/updateAirpotRef"})
-    public String updateAirpotRef() {
-        return("Change ref of airport");
+        return("{\"message\":\"Delete entire airport list\"}");
     }
+    // delete entire array of airports (200-204)
 
-    @DeleteMapping(value = {"/deleteAirport"})
+    @DeleteMapping(value = {"/airports/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public String deleteAirport() {
-        return("Delete specific airport");
+
+        return("{\"message\":\"Delete specific airport\"}");
     }
+    // deleting a specific airport or airport data field (200-204)
 }
--- springAPI-application/src/test/java/com/example/demo/AirportAPITest.java
+++ springAPI-application/src/test/java/com/example/demo/AirportAPITest.java
@@ -1,13 +1,81 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.Base64Utils;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**
+ * JUnit to test the Hello World logic
+ */
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AirportAPITest {
-
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AirportAPITest {
+	private static final ObjectMapper om = new ObjectMapper();
+	@Autowired
+	private MockMvc mockMvc;
+	@Before
+	public void init() {
+	}
 	@Test
-	void contextLoads() {
+	public void siteHome() throws Exception {
+		mockMvc.perform(get("/").header(HttpHeaders.AUTHORIZATION,
+				"Basic " + Base64Utils.encodeToString("admin:nimda".getBytes())))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("message", is("Welcome to the Global Airports System")));
 	}
 
+	@Test
+	public void getAirports() throws Exception {
+		mockMvc.perform(get("/airports").header(HttpHeaders.AUTHORIZATION,
+				"Basic " + Base64Utils.encodeToString("admin:nimda".getBytes())))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("message", is("View all airports data")));
+	}
+
+	@Test
+	public void getAirport() throws Exception {
+		mockMvc.perform(get("/airports/{id}").header(HttpHeaders.AUTHORIZATION,
+				"Basic " + Base64Utils.encodeToString("admin:nimda".getBytes())))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("message", is("You are now viewing {id}")));
+	}
+
+	@Test
+	public void addAirport() throws Exception {
+		mockMvc.perform(post("/airports").header(HttpHeaders.AUTHORIZATION,
+				"Basic " + Base64Utils.encodeToString("admin:nimda".getBytes())))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("message", is("Please select which airport to add to the system")));
+	}
+
+	 /** private static void printJSON(Object object) {
+		String result;
+		try {
+			result = om.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+			System.out.println(result);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	} */
 }
